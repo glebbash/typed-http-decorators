3052402984996f7cd437e2de5dceb4c473cf72ce
glebbash <glebbash@gmail.com>
2022-03-21 09:54:11 +0000

    feat: add either function as alias to t

--- src/typed-http-decorators.ts
+++ src/typed-http-decorators.ts
@@ -8,3 +8,4 @@ export * as Method from './methods';
 export * from './responses';
 export * from './types';
 export const t = <XS extends readonly unknown[]>(...values: XS): XS => values;
+export const either = t;
